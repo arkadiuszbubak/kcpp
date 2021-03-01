6b8798f3a262c57f1e7dc474ec434336ff149c1c
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-03-01 12:43:08 +0100

    Wygładzanie kodu

--- kcppBasic/src/LSizeof.cc
+++ kcppBasic/src/LSizeof.cc
@@ -10,25 +10,25 @@ using namespace std;
 
 int main()
 {
-int mm ;
+  int mm ;
 
-        cout << "Godzina prawdy. W tym komputerze "
-                << "poszczegolne typy\n"
-                << "maja nastepujace rozmiary w bajtach: \n"  ;
+  cout << "Godzina prawdy. W tym komputerze "
+    << "poszczegolne typy\n"
+    << "maja nastepujace rozmiary w bajtach: \n"  ;
 
-        cout << "typ char        : \t" << sizeof(char) << endl ;
-        cout << "typ int         : \t" << sizeof(int) << endl ;
-        cout << "typ short       : \t" << sizeof(short) << endl ;
-        cout << "typ long        : \t" << sizeof(long) << endl ;
-        cout << "typ float       : \t" << sizeof(float) << endl ;
-        cout << "typ double      : \t" << sizeof(double) << endl ;
-        cout << "typ long double : \t"<< sizeof(long double)
-                                                        << endl ;
+  cout << "typ char        : \t" << sizeof(char) << endl ;
+  cout << "typ int         : \t" << sizeof(int) << endl ;
+  cout << "typ short       : \t" << sizeof(short) << endl ;
+  cout << "typ long        : \t" << sizeof(long) << endl ;
+  cout << "typ float       : \t" << sizeof(float) << endl ;
+  cout << "typ double      : \t" << sizeof(double) << endl ;
+  cout << "typ long double : \t"<< sizeof(long double)
+    << endl ;
 
-        cout << "Nasz obiekt lokalny mm ma rozmiar : "
-                 << sizeof(mm) << endl ;
+  cout << "Nasz obiekt lokalny mm ma rozmiar : "
+    << sizeof(mm) << endl ;
 
 
-return 0;                 
-                 
+  return 0;                 
+
 }
