662ae9c3ae9ebd18f540df45390ab6c8dcb5bce8
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-03-03 09:24:13 +0100

    Cleaning

--- kcppBasic/src/LSizeof.cc
+++ kcppBasic/src/LSizeof.cc
@@ -16,13 +16,13 @@ int main()
     << "poszczegolne typy\n"
     << "maja nastepujace rozmiary w bajtach: \n"  ;
 
-  cout << "typ char        : \t" << sizeof(char) << endl ;
-  cout << "typ int         : \t" << sizeof(int) << endl ;
-  cout << "typ short       : \t" << sizeof(short) << endl ;
-  cout << "typ long        : \t" << sizeof(long) << endl ;
-  cout << "typ float       : \t" << sizeof(float) << endl ;
-  cout << "typ double      : \t" << sizeof(double) << endl ;
-  cout << "typ long double : \t" << sizeof(long double) << endl ;
+  cout << "typ char        : \t" << sizeof(char) << endl;
+  cout << "typ int         : \t" << sizeof(int) << endl;
+  cout << "typ short       : \t" << sizeof(short) << endl;
+  cout << "typ long        : \t" << sizeof(long) << endl;
+  cout << "typ float       : \t" << sizeof(float) << endl;
+  cout << "typ double      : \t" << sizeof(double) << endl;
+  cout << "typ long double : \t" << sizeof(long double) << endl;
 
   cout << "Nasz obiekt lokalny mm ma rozmiar : "
     << sizeof(mm) << endl ;
