dc3cff39bd5123012e07c8a986b94f0a8ff6945c
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-03-12 10:40:58 +0100

    Changed order of variable inicialization

--- kcppBasic/src/LArytmetykaWskaznikowZmienne.cc
+++ kcppBasic/src/LArytmetykaWskaznikowZmienne.cc
@@ -13,11 +13,11 @@
 using namespace std;
 
 int main() { 
-	int *wsk_int;
-	float x;
-	int c;
-	int b;
 	int a;
+	int b;
+	int c;
+	float x;
+	int *wsk_int;
 	//Rozne sposoby zapisu nowej wartosci do zmiennej b za pomoca wskaznika na b i sasiadujace zmienne
 
 	// Wyswieltlenie adresow zmiennych
@@ -74,19 +74,19 @@ int main() {
 	cout <<"3. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
 	wsk_int = &a;
-	*(wsk_int-1) = 30;
+	*(wsk_int+1) = 30;
 	cout <<"4. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
-	*(&a - 1) = 40; 
+	*(&a + 1) = 40; 
 	cout <<"5. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
-	*(&c + 1) = 50;
+	*(&c - 1) = 50;
 	cout <<"6. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
-	*( (int*)&x + 2) = 60;
+	*( (int*)&x - 2) = 60;
 	cout <<"7. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
-	*(int*)( &x + 1) = 70;
+	*(int*)( &x - 1) = 70;
 	cout <<"8. A, B, C: "<< a <<"\t"<< b <<"\t"<< c << endl;
 
 	*((int*)&wsk_int - 2) = 80;
