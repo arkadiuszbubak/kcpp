f3edca7e7a7bd099e624418b43a6bf1051ceb295
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-03-18 11:59:50 +0100

    Added description and cout

--- kcppBasic/src/LOperatorPrzypisania.cc
+++ kcppBasic/src/LOperatorPrzypisania.cc
@@ -11,30 +11,32 @@ using namespace std;
 
 int main() {
 
-    int
-       varA = 5,
-       varB = 27;
+  int
+    varA = 5,
+         varB = 27;
 
-    float result[10];
+  float result[10];
 
-    result[0] = varB/varA;
-    result[1] = varB%varA;
+  result[0] = varB/varA;
+  result[1] = varB%varA;
 
-    if(varB%varA != 0) cout << "bleee bleee" << endl;
+  if(varB%varA != 0) cout << "bleee bleee" << endl;
 
 
-    cout << "Rezultaty: "
-        << result[0] << "\t"
-        << result[1] << endl;
+  cout << "Rezultaty: "
+    << result[0] << "\t"
+    << result[1] << endl;
 
-    
-    /* for(int i = 0; i < 100; i++){
-     *    sleep(1);
-     *    if(i%10 == 0){
-     *        cout <<"#"<<i<< endl;
-     *    }
-     * } */
-    
 
-    return 0;   
+  /* //Application example    
+   * for(int i = 0; i < 100; i++){
+   *   sleep(1);
+   *   cout <<"---->"<< i << endl;
+   *   if(i%10 == 0){
+   *     cout <<"#"<<i<< endl;
+   *   }
+   * } */
+
+
+  return 0;   
 }
