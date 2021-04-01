4f894d5b3599d3714098d58932237d9ba74e422e
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-01 10:27:45 +0200

    Improved indentation. Added comment

--- kcppBasic/src/LInstrukcjaGoto.cc
+++ kcppBasic/src/LInstrukcjaGoto.cc
@@ -3,7 +3,7 @@
  * \date 2019.04.049
  * \brief instrukcja goto w C++
  * v0.01
-*/
+ */
 
 #include <iostream>
 #include <cmath>
@@ -11,13 +11,13 @@ using namespace std;
 
 int main()
 {
-   tryAgain:           // etykieta
-   cout << "Wpisz dodatnia liczbe: ";
+tryAgain:           // etykieta
+  cout << "Wpisz dodatnia liczbe: ";
 
-   double dX;
-   cin >> dX;
+  double dX;
+  cin >> dX;
 
-   if (dX < 0.0) goto tryAgain;
+  if (dX < 0.0) goto tryAgain;
 
-   cout << "The sqrt of " << dX << " is " << sqrt(dX) << endl;
+  cout << "The sqrt of " << dX << " is " << sqrt(dX) << endl;
 }
