4d7757f1ac6fa34855008d3663662750fcfffd27
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-01 10:33:58 +0200

    Added cout and imporved indentation

--- kcppBasic/src/LInstrukcjaWhile.cc
+++ kcppBasic/src/LInstrukcjaWhile.cc
@@ -16,13 +16,16 @@ int main()
   while(i < 10) i++; ///< po wykonaniu petli i = 10
   cout << i << endl;
 
-  while(i++) if(i==15) break; ///< jesli wynosi 15 to przerywamy wykonywanie petli
+  while(i++){
+    cout << i << " ";
+    if(i==15) break; ///< jesli wynosi 15 to przerywamy wykonywanie petli
+  }
 
   while(true) goto etykieta1; ///< skok do etykiety
   cout <<"Czy kiedykolwiek sie pojawie?"<< endl;
 
 etykieta1:
-
+  cout << endl;
   while(i <= 20) { ///< wyswietla liczby 15, 16, 17, 19 i 20
     if(i==18) {
       i++;
