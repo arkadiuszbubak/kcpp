00e70e3aa0ba2cade0bae1b1be7218b733d0170f
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-01 10:18:32 +0200

    Improved indentation

--- kcppBasic/src/LInstrukcjaSwitch_char_A.cc
+++ kcppBasic/src/LInstrukcjaSwitch_char_A.cc
@@ -11,25 +11,25 @@ using namespace std;
 int main()
 {
 
-   //Uzycie tylko duzych liter   
-    char var = 0;
-   
-    cout << "Wybierz: [O]la || [M]arzena || [A]nia: ";
-    cin >> var;
-   
-    switch(var) {	
-      case 'O':
-         cout << "Wybrano Ole" << endl;
-	 break;
-      case 'M':
-         cout << "Wybrano Marzene" << endl;
-	 break;
-      case 'A':
-         cout << "Wybrano Anie" << endl;
-	 break;
-      default:
-         cout << "Nie wybrano zadnej dziewczyny" << endl;
-     }
-   
-    return 0;
+  //Uzycie tylko duzych liter   
+  char var = 0;
+
+  cout << "Wybierz: [O]la || [M]arzena || [A]nia: ";
+  cin >> var;
+
+  switch(var) {	
+    case 'O':
+      cout << "Wybrano Ole" << endl;
+      break;
+    case 'M':
+      cout << "Wybrano Marzene" << endl;
+      break;
+    case 'A':
+      cout << "Wybrano Anie" << endl;
+      break;
+    default:
+      cout << "Nie wybrano zadnej dziewczyny" << endl;
+  }
+
+  return 0;
 }
