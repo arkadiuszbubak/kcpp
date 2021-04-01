a42d347d5b6a9b455474899f4743a50ae1ed0e72
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-01 09:06:11 +0200

    Change the precision of displaying decimal places (20: long double). Added description

--- kcppBasic/src/LSetPrecision.cc
+++ kcppBasic/src/LSetPrecision.cc
@@ -7,18 +7,24 @@
 
 #include <iostream>
 #include <iomanip>
-//#include <math.h>
+#include <math.h>
 using namespace std;
 
 int main(){
 
-	double f = 3.141592653589793238462643;
-	//double f = M_PI; // wykorzystanie pi z biblioteki math.h
+	//double f = 3.141592653589793238462643;
+	long double f = M_PI; // wykorzystanie pi z biblioteki math.h
 	cout << setprecision (5) << f << endl;
 	cout << setprecision (9) << f << endl;
 	cout << fixed;
 	cout << setprecision (5) << f << endl;
-	cout << setprecision (52) << f << endl;
+	cout << setprecision (20) << f << endl;
+	//cout << setprecision (60) << M_PI << endl;
+
+  /* float   4   pojedyncza precyzja - dokładność 6 - 7 cyfr po przecinku
+   * double  8   podwójna precyzja - dokładność 15 - 16 cyfr po przecinku
+   * long double   12  liczby z ogromną dokładnością - 19 - 20 cyfr po przecinku */
+
 
 	return 0;
 }
