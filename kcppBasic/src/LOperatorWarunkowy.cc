41964f0df36444489908f50d7f5c713b3f6b63b9
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-01 10:10:14 +0200

    Improve indentation

--- kcppBasic/src/LOperatorWarunkowy.cc
+++ kcppBasic/src/LOperatorWarunkowy.cc
@@ -10,16 +10,16 @@ using namespace std;
 
 int main()
 {
-    int x;
-    cout << "Podaj liczbe calkowita:" << endl;
-    cin >> x;
-    cout << ( (x & 1) ? "Liczba nieparzysta." : "Liczba parzysta.") << endl;
-    
-    if((x & 1) ? 0 : 1) {
-      cout << "Liczba parzysta -> w if"<< endl;
-     } else {
-      cout << "Liczba nieparzysta -> w else"<< endl;
-     }
+  int x;
+  cout << "Podaj liczbe calkowita:" << endl;
+  cin >> x;
+  cout << ( (x & 1) ? "Liczba nieparzysta." : "Liczba parzysta.") << endl;
 
-    return 0;
+  if((x & 1) ? 0 : 1) {
+    cout << "Liczba parzysta -> w if"<< endl;
+  } else {
+    cout << "Liczba nieparzysta -> w else"<< endl;
+  }
+
+  return 0;
 }
