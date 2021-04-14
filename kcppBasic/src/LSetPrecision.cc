da053cd82a1e47772f34c921f65788e0a43ae206
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-14 10:27:58 +0200

    polishing

--- kcppBasic/src/LSetPrecision.cc
+++ kcppBasic/src/LSetPrecision.cc
@@ -14,12 +14,13 @@ using namespace std;
 int main(){
 
 	//double f = 3.141592653589793238462643;
-	long double f = M_PI; // wykorzystanie pi z biblioteki math.h
+	double f = 3.14159;
+	//long double f = M_PI; // wykorzystanie pi z biblioteki math.h
 	cout << setprecision (5) << f << endl;
 	cout << setprecision (9) << f << endl;
 	cout << fixed;
 	cout << setprecision (5) << f << endl;
-	cout << setprecision (20) << f << endl;
+	cout << setprecision (9) << f << endl;
 	//cout << setprecision (60) << M_PI << endl;
 
   /* float   4   pojedyncza precyzja - dokładność 6 - 7 cyfr po przecinku
