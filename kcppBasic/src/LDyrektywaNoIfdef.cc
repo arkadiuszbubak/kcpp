6835aee8018f67268b694fb717f2d621e30583cb
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-04-21 10:49:48 +0200

    Lack of Ifdef directive in comparison to file LDyrektywaIfdef.cc

---
+++ kcppBasic/src/LDyrektywaNoIfdef.cc
@@ -0,0 +1,19 @@
+/**
+ *\author: Arkadiusz Bubak arkadiusz@bubak.pl
+ * \date 2021.04.21
+ * \brief Dyrektywa 'ifdef' w C++
+ * v0.01
+ */
+
+#include <iostream>
+
+int main()
+{
+
+  std::cout << "A: yes\n";
+
+  std::cout << "B: yes\n";
+
+  std::cout << "C: yes\n";
+
+}
