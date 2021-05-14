952fe4c37dfd8b3ff76696aae606da14cde15c7a
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-05-14 13:40:05 +0200

    Additional info

--- kcppBasic/src/LClassDziedziczenieA.cc
+++ kcppBasic/src/LClassDziedziczenieA.cc
@@ -14,7 +14,7 @@ class Przodek
    int FunA(int zm);
 
    int   fM;
-   float fN;
+   float fN;   ///< fN ia only here in the class you inherit from
 };
 
 class Potomek : public Przodek 
@@ -27,7 +27,7 @@ class Potomek : public Przodek
 
 int Przodek::FunA(int zm)
 {
-   int result = zm + 3;
+   int result = zm + fN;
    return result;
 }
 
@@ -37,22 +37,33 @@ int main()
 {
    Potomek potObiekt;
    potObiekt.fN = 8;
-   cout << potObiekt.fN << endl;
+   cout <<"potObiekt.fN: "<< potObiekt.fN << endl;
    
    //------------ 1 ------------------------
    Przodek przObiekt;
    przObiekt.fN = 7;
-   cout << przObiekt.fN << endl;
+   cout <<"przObiekt.fN: "<<przObiekt.fN << endl;
    
    //------------ 2 ------------------------
-   cout << potObiekt.fN << endl;
+   cout <<"potObiekt.fN: "<< potObiekt.fN << endl;
    
    //------------ 3 ------------------------
-   int resA = przObiekt.FunA(7);
+   int resA = przObiekt.FunA(3);
    cout << resA << endl;
 
    //------------ 4 ------------------------
-   int resB = potObiekt.FunA(8);
-   cout << resB << endl;  
+   int resB = potObiekt.FunA(3);
+   cout << resB << endl;
+
+
+   //------------ 5 ------------------------
+   potObiekt.fM = 3;
+   cout <<"potObiekt.fM: "<< potObiekt.fM << endl;
+   przObiekt.fM = 7;
+
+   cout <<"przObiekt.fM: "<< przObiekt.fM << endl;
+   cout <<"potObiekt.fM: "<< potObiekt.fM << endl;
+
+
 }
 
