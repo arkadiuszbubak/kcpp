240b361d8a675dd4a766d36c04205740f39d6ad6
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-05-19 10:13:10 +0200

    Working...

--- kcppBasic/src/LClassFunkcjeZaprzyjaznione.cc
+++ kcppBasic/src/LClassFunkcjeZaprzyjaznione.cc
@@ -10,33 +10,36 @@ using namespace std;
 
 class Pudelko 
 {   
- public:
-   Pudelko(float d, float s, float w) : fD(d), fS(s), fW(w) { }
-   //int GetValueD() { return fD; }; 
-   friend int Equalsize(Pudelko a, Pudelko b);
-   
- private:
-     float fD, fS, fW;
+  public:
+    Pudelko(float d, float s, float w) : fD(d), fS(s), fW(w) { }
+    //float GetValueD() { return fD; }; 
+    friend int Equalsize(Pudelko a, Pudelko b);
+
+  private:
+    float fD, fS, fW;
 };
 
 
 int Equalsize(Pudelko a, Pudelko b) {
 
-   // Funkcja nie bedaca metoda klasy Pudelko korzysta ze
-   // skladowych prywatnych klasy Pudelko
+  // Funkcja nie bedaca metoda klasy Pudelko korzysta ze
+  // skladowych prywatnych klasy Pudelko
 
   //if( a.GetValueD() == b.GetValueD()) return 1;    // A czy to zadziała?
   if( a.fD * a.fS * a.fW == b.fD * b.fS * b.fW) return 1;
   else return 0;
- 
+
 }
 
 int main()
 {
-   Pudelko objA(1, 2, 3);
-   Pudelko objB(1, 2, 3);    // 1.
-   //Pudelko objB(1, 4, 3);  // 2. Linie 1.to zamienmy na linie 2.
+  Pudelko objA(1, 2, 3);
+  Pudelko objB(1, 2, 3);    // 1.
+  //Pudelko objB(1, 4, 3);  // 2. Linie 1.to zamienmy na linie 2.
 
-   cout << Equalsize(objA, objB) << endl;
+  //cout << objA.fD << endl;
+  //cout << objA.GetValueD() << endl;
+
+  cout << Equalsize(objA, objB) << endl;
 }
 
