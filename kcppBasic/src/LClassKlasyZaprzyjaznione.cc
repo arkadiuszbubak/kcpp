b3ba2b12cb776a155c3bb0a1a23b48f3a1906796
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-05-19 14:14:06 +0200

    Cleaning...

--- kcppBasic/src/LClassKlasyZaprzyjaznione.cc
+++ kcppBasic/src/LClassKlasyZaprzyjaznione.cc
@@ -9,61 +9,54 @@
 using namespace std;
 
 //-----------------------------------------------------------
-class A;
-class B;
-
 class A 
 {
- public:
-   void SetValue(int A, int B) {
+  public:
+    void SetValue(int A, int B) {
       fZmiennaA = A;
       fZmiennaB = B;
-     }
-   void SetABZero() {
-     fZmiennaA = fZmiennaB = 4;
-   }
-   void GetB(B &obj) {
-     cout << obj.fB << endl;
-     }
-   
-   friend void Pokaz(A&);
-   friend class B;
+    }
+    void SetABZero() {
+      fZmiennaA = fZmiennaB = 4;
+    }
 
- private:
-   int fZmiennaA, fZmiennaB;
+    friend void Pokaz(A&);
+    friend class B;
+
+  private:
+    int fZmiennaA, fZmiennaB;
 
 };
 
 //-----------------------------------------------------------
 class B 
 {
- public:
-   void ZerujA(A &obj) {
-     obj.fZmiennaA = obj.fZmiennaB = 0;
+  public:
+    void ZerujA(A &obj) {
+      obj.fZmiennaA = obj.fZmiennaB = 0;
       //obj.SetABZero(); //Gdy class B nie jest zaprzyjaźniona 
-   }
+    }
 
-  private:
-    int fB;
-
-   //friend class A;
 };
+
 
 //-----------------------------------------------------------
 void Pokaz(A &obj){
-   cout << obj.fZmiennaA << " " << obj.fZmiennaB << endl;;
+  cout << obj.fZmiennaA << " " << obj.fZmiennaB << endl;;
 }
 
+
+
+//-----------------------------------------------------------
 int main()
 {
-   A objA;
-   objA.SetValue(7,8);
-   Pokaz(objA);
+  A objA;
+  objA.SetValue(7,8);
+  Pokaz(objA);
 
 
-   B objB;
-   objB.ZerujA(objA);
-   Pokaz(objA);
-   
-   //objA.GetB(objB);
+  B objB;
+  objB.ZerujA(objA);
+  Pokaz(objA);
+
 }
