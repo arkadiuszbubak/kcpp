ee9864cba03ff75f870d077d1e501a895beb9550
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2021-06-01 11:18:59 +0200

    Cleaning. To 'int' and 'float' added 'double' function.

--- kcppBasic/src/noTemplateFunction.cc
+++ kcppBasic/src/noTemplateFunction.cc
@@ -9,42 +9,51 @@
 using namespace std;
 
 
-/*   // ----- Wersja 2 -----------
- *   int AddInt(int t1, int t2) {
- *      return t1 +t2;
- *   }
+/* // ----- Wersja 2 -----------
+ * int AddInt(int t1, int t2) {
+ *   return t1 + t2;
+ * }
  *
- *   float AddFloat(float t1, float t2) {
- *    return t1 +t2;
- *  //----------------------------*/
+ * float AddFloat(float t1, float t2) {
+ *   return t1 + t2;
+ * }
+ * //---------------------------- */
 
 
 
-/*   // ----- Wersja 1 -----------
- *  int Add(int t1, int t2) {
- *      return t1 +t2;
- *   }
- *
- *   float Add(float t1, float t2) {
- *      return t1 +t2;
- *   }
- *  //----------------------------*/
+// ----- Wersja 1 -----------
+int Add(int t1, int t2) {
+  return t1 + t2;
+}
+
+float Add(float t1, float t2) {
+  return t1 + t2;
+}
+//double Add(double t1, double t2) {
+//  return t1 + t2;
+//}
+//----------------------------
 
 
 int main() {
-   
-    
-   /* // ----- Wersja 2 -----------
-    * cout << AddInt(2,3)       << endl;
-    * cout << AddFloat(2.6,3.2) << endl;  // wywolanie szablonu max<double> (dedukcja typu argumentów)
-    * cout << AddInt(2,13)      << endl;  // wywolanie szablonu max<int> (dedukcja typu argumentów)
-    * cout << AddFloat(2,14)    << endl;  // wywolanie szablonu max<double> (bez dedukcji typu)
-    * //--------------------------- */
 
-  /*  // ----- Wersja 1 -----------
-   *  cout << Add(2,3)     << endl;
-   *  cout << Add(2.6,3.2) << endl;  // wywolanie szablonu max<double> (dedukcja typu argumentów)
-   *  cout << Add(2,13)    << endl;  // wywolanie szablonu max<int> (dedukcja typu argumentów)
-   *  cout << Add(2,14)    << endl;  // wywolanie szablonu max<double> (bez dedukcji typu)
-   * //---------------------------- */
+
+  /* // ----- Wersja 2 -----------
+   * cout << AddInt(2,3)       << endl;
+   * cout << AddFloat(2.6,3.2) << endl;  
+   * cout << AddInt(2,13)      << endl;  
+   * cout << AddFloat(2,14)    << endl; 
+   * //--------------------------- */
+
+  // ----- Wersja 1 -----------
+  cout << Add(2,3)     << endl;
+  //cout << Add(2.6,3.2) << endl;  
+  cout << Add(2,13)    << endl;  
+  cout << Add(2,14)    << endl;  
+  //----------------------------
+  
+
+  /* cout <<"Int size:    "<< sizeof(int) << endl;
+   * cout <<"Float size:  "<< sizeof(float) << endl;
+   * cout <<"Double size: "<< sizeof(double) << endl; */
 }
