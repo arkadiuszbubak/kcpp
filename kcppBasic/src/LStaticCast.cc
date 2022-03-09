2027f2684c3805080e6f1925078ed2927d86857c
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2022-03-09 12:16:22 +0100

    Working during classes

--- kcppBasic/src/LStaticCast.cc
+++ kcppBasic/src/LStaticCast.cc
@@ -24,34 +24,34 @@ int main(){
 	cout <<" 2: "<< (double)(a)/b << endl;
 	cout <<" 3: "<< (double)(a/b) << endl;
 	cout <<" 4: "<< (double) a/b << endl;
-  // cout <<"5: "<< double(a)/b  << endl;
-	// cout <<"6: "<< a/double(b)  << endl;
+  //cout <<" 5: "<< double(a)/b  << endl;
+	//cout <<" 6: "<< a/double(b)  << endl;
 	//------------------------------------------
 
-	//------------------------------------------
-	double c1 = static_cast<double>(a) / static_cast<double>(b);
-	double c2 = static_cast<double>(a) /b;
-	double c3 = static_cast<double>(a/b);
-  //double c4 = static_cast<double> a/b; //ERROR
-	//------------------------------------------
-  std::cout << std::fixed << std::setprecision(2) <<"c1: "<< c1 << endl;
-  std::cout << std::fixed << std::setprecision(2) <<"c2: "<< c2 << endl;
-  std::cout << std::fixed << std::setprecision(2) <<"c3: "<< c3 << endl;
-
+/*   //------------------------------------------
+ *   double c1 = static_cast<double>(a) / static_cast<double>(b);
+ *   double c2 = static_cast<double>(a) /b;
+ *   double c3 = static_cast<double>(a/b);
+ *   //double c4 = static_cast<double> a/b; //ERROR
+ *   //------------------------------------------
+ *   std::cout << std::fixed << std::setprecision(2) <<"c1: "<< c1 << endl;
+ *   std::cout << std::fixed << std::setprecision(2) <<"c2: "<< c2 << endl;
+ *   std::cout << std::fixed << std::setprecision(2) <<"c3: "<< c3 << endl;
+ *  */
   
-	//------------------------------------------
-  cout << endl;
-  
-  int i = 6;
-	double d1 = (double)i;               //C-style cist
-	cout <<"d1: "<< d1 << endl;
-
-	double d2 = static_cast<double>(i);  //C++ cast
-	cout <<"d2: "<< d2 << endl;
-
-	double d = 3.14159265;
-	int    k = static_cast<int>(d);
-	cout <<"k: "<< k << endl;
-
+/*   //------------------------------------------
+ *   cout << endl;
+ *
+ *   int i = 6;
+ *   double d1 = (double)i;               //C-style cist
+ *   cout <<"d1: "<< d1 << endl;
+ *
+ *   double d2 = static_cast<double>(i);  //C++ cast
+ *   cout <<"d2: "<< d2 << endl;
+ *
+ *   double d = 3.14159265;
+ *   int    k = static_cast<int>(d);
+ *   cout <<"k: "<< k << endl;
+ *  */
 	return 0;
 }
