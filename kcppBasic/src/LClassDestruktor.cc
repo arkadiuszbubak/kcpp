d601ae810384fcbe6c959fd7c532ef65cf08c243
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2022-05-04 13:09:37 +0200

    Added explanation when destructor is called in case: static and dynamic

--- kcppBasic/src/LClassDestruktor.cc
+++ kcppBasic/src/LClassDestruktor.cc
@@ -33,7 +33,13 @@ int  main(){
    A *obiektC = new A(444);
    A *obiektD = new A(444.1);
    A *obiektE = new A();
-        
+  
+   A obiektF;
+   /*  Destructor is called automaticaly then:
+    *  1. The program finished execution.
+    *  2. When a scope (the { } parenthesis) containing local variable ends.
+    *  3. When you call the delete operator */
+
    cout << obiektC->fLicznikInt    << endl; 
    cout << obiektD->fLicznikDouble << endl; 
    cout << obiektE->fLicznikInt    << endl; 
