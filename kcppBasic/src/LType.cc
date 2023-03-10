2ab9f2de5bc1d32bb760fb28eb8cb32604a606dc
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2023-03-10 14:27:53 +0100

    Working during class

--- kcppBasic/src/LType.cc
+++ kcppBasic/src/LType.cc
@@ -11,22 +11,22 @@ using namespace std;
 int main()
 {
   int fVarA;
-  fVarA = 7;
+  //fVarA = 7;
   fVarA = 7.3;
 
   cout <<"fVarA: "<< fVarA << endl;
 
   //float fVarA;              //Error
-  //float fVarB;
-  //fVarB = 7;
-  //fVarB = 7.3;
-  //cout <<"fVarB: "<< fVarB << endl;
+  // float fVarB;
+  // fVarB = 7;
+  // fVarB = 7.3;
+  // cout <<"fVarB: "<< fVarB << endl;
 
 
   char fVarC[10];   //deklaracja stringu o długości 10 znaków
   char fVarD[] = "Tutaj jakis napis";  /*deklaracja napisu (następuje automatyczne przypisanie tekstu zmiennej oraz określenie jej długości) */
-  //char *fVarE = "A tutaj inny napis";  //podobne do powyższej metody, deprecated
-  //char fVarF = 'Inny'; // warning: multi-character constant; only one character 'I'
+  // char *fVarE = "A tutaj inny napis";  //podobne do powyższej metody, deprecated
+  // char fVarF = 'Inny'; // warning: multi-character constant; only one character 'I'
 
   /* cout << "Set fCharC variable" << endl;
    * cin >> fVarC; */
@@ -34,8 +34,8 @@ int main()
 
   cout << "fVarC: "<< fVarC << endl;
   cout << "fVarD: "<< fVarD << endl;
-  //cout << "fVarE: "<< fVarE << endl;
-  //cout << "fVarF: "<< fVarF << endl;
+  // cout << "fVarE: "<< fVarE << endl;
+  // cout << "fVarF: "<< fVarF << endl;
 
   return 0;                              
 }
