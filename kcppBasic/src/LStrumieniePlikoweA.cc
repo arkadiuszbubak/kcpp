9864b349432b0330ba71da8e1bdf6437625f91bf
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2023-04-27 10:22:48 +0200

    Added reading of the entire stream

--- kcppBasic/src/LStrumieniePlikoweA.cc
+++ kcppBasic/src/LStrumieniePlikoweA.cc
@@ -1,8 +1,8 @@
 /**
  * \author: Arkadiusz Bubak arkadiusz@bubak.pl
- * \date 2019.02.10
+ * \date 2019.02.10 v0.01; 2023.04.27 v0.02
  * \brief Strumienie w C++
- * v0.01
+ * v0.02
  */
 
 #include <iostream>
@@ -10,16 +10,23 @@
 #include <string>
 using namespace std;
 
+
 int main()
 {
-   string a;
-   cout << "Nacisnij Enter aby zakonczyc zapis.\n";
-   ofstream f("log.txt");
-   cin >> a;
-   if (f.good())
-   {
-      f << a;
-      f.close();
-   }
-   return 0;
+  ofstream f("log.txt");
+
+  string fullName;
+
+  cout << "Type your full name (then press Enter): \n";
+  cin >> fullName;
+  //getline (cin, fullName);
+
+  cout << "Your name is: " << fullName << endl;;
+
+  if (f.good())
+  {
+    f << fullName;
+    f.close();
+  }
+  return 0;
 }
