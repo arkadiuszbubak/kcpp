20d50507c30048127d2c795f66eed6ca573e56f0
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2023-05-10 14:32:33 +0200

    Added description

--- kcppBasic/src/LSetPrecision.cc
+++ kcppBasic/src/LSetPrecision.cc
@@ -3,7 +3,7 @@
  * \date 2019.02.10
  * \brief Manipulatory: setprecision w C++
  * modified from: https://www.cplusplus.com/reference/iomanip/setprecision/
- * v0.01
+ * v0.02 (2023.05.10)
  */
 
 #include <iostream>
@@ -19,11 +19,11 @@ int main(){
 	long double f = M_PI; // wykorzystanie pi z biblioteki math.h
   cout << f << endl;
 	cout << fixed;
-	cout << setprecision (5) << f << endl;
-	cout << setprecision (9) << f << endl;
-	cout << scientific;
-	cout << setprecision (5) << f << endl;
-	cout << setprecision (9) << f << endl;
+	cout <<"Set precision: 5\t"<< setprecision (5) << f << endl;
+	cout <<"Set precision: 9\t"<< setprecision (9) << f << endl;
+	cout <<"Set scientific"<< scientific << endl;;
+	cout <<"Set precision: 5\t"<< setprecision (5) << f << endl;
+	cout <<"Set precision: 9\t"<< setprecision (9) << f << endl;
 	//cout << setprecision (60) << M_PI << endl;
 
   /* float   4   pojedyncza precyzja - dokładność 6 - 7 cyfr po przecinku
