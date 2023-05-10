1571a30e7d8761307b175cd25634a6a592cdb29c
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2023-05-10 14:53:12 +0200

    Added decription in cout

--- kcppBasic/src/LManipulatory.cc
+++ kcppBasic/src/LManipulatory.cc
@@ -11,16 +11,19 @@ using namespace std;
 
 int main(){
 
-	int x = 7;
-	float y = 8.123456789;
+  int x = 7;
+  float y = 8.123456789;
 
-	cout << x << endl;
-	cout << setfill ('x') << setw (10);
-	cout << setw (10) << y << endl;
-	cout << y << endl;
-  cout <<"  "<< y << endl;
-  cout <<"\t"<< y << endl;
+  cout <<"1: "<<x << endl;
+  cout << setfill('x') << setw (10);
+  cout << setw(10) << y << endl;
+  cout << y << endl;
+  cout <<"4: "<<" "<< y << endl;
+  cout <<"5: "<<"\t"<< y << endl;
 
+  /* float   4   pojedyncza precyzja - dokładność 6 - 7 cyfr po przecinku
+   * double  8   podwójna precyzja - dokładność 15 - 16 cyfr po przecinku
+   * long double   12  liczby z ogromną dokładnością - 19 - 20 cyfr po przecinku */
 
-	return 0;
+  return 0;
 }
