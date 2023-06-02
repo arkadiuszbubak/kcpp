888bfda64227d00b611c0e0e4435d1c75279d774
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2023-06-02 15:34:33 +0200

    Added mix of classic constructor and constructor with initialization list

--- kcppBasic/src/LClassListaInicjalizacyjna.cc
+++ kcppBasic/src/LClassListaInicjalizacyjna.cc
@@ -16,15 +16,22 @@ class A
     * }; */
    A(int zmiennaA, int zmiennaB);
    A(int zmiennaA, int zmiennaB, int zmiennaC);
+   A(int zmiennaA, int zmiennaB, float zmiennaD);
    
    int fLicznikA;
    int fLicznikB;
    int fLicznikC;
+   float fLicznikD;
 };
 
 //--- Lista inicializacyjna -------
 A::A(int zmiennaA, int zmiennaB) : fLicznikA(zmiennaA), fLicznikB(zmiennaB)
 {}
+//Strange - useful?
+A::A(int zmiennaA, int zmiennaB, float zmiennaD) : fLicznikA(zmiennaA), fLicznikB(zmiennaB)
+{
+  fLicznikD = zmiennaD;
+}
 
 //--- 'Standardowy konstruktor' ---
 A::A(int zmiennaA, int zmiennaB, int zmiennaC){
@@ -36,18 +43,21 @@ A::A(int zmiennaA, int zmiennaB, int zmiennaC){
 
 int main()
 {
-   A *obiektN = new A(7, 4, 5);
+   int zm = 5;
+   A *obiektN = new A(7, 4, zm);
    cout << obiektN->fLicznikA <<"\t"
         << obiektN->fLicznikB <<"\t"
         << obiektN->fLicznikC << endl;;
-   
+  
+   float zn = 7.1;
+   A *obiektO = new A(3, 4, zn);
    A *obiektM = new A(3, 6);
    cout << obiektM->fLicznikA <<"\t"
         << obiektM->fLicznikB <<"\t"
         << obiektM->fLicznikC <<"\t"
         << endl;
-   /* A *obiekt1 = new A();
-    * A obiekt2; */
+   // A *obiekt1 = new A();
+   // A obiekt2;
 
 }   
 
