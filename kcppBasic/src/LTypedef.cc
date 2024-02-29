0d1235d1a7064726d25a1387c9e0acb458a5369f
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2024-02-29 11:36:15 +0100

    Typedef example

---
+++ kcppBasic/src/LTypedef.cc
@@ -0,0 +1,18 @@
+/**
+ * \author: Arkadiusz Bubak arkadiusz@bubak.pl
+ * \date 2024.02.29
+ * \brief Aliasy Typedef
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  typedef int LInt;
+  LInt zmienna = 8;
+  cout << zmienna << endl;
+
+  return 0;                              
+}
