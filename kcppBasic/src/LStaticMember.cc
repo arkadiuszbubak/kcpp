1bd49c5462e65d3336dc246eb469d30a964b9dfd
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2024-04-10 10:54:04 +0200

    .

--- kcppBasic/src/LStaticMember.cc
+++ kcppBasic/src/LStaticMember.cc
@@ -12,14 +12,14 @@ class LBox {
   public:
     static int iObjectCount;
 
-    // Constructor definition
+    //! Constructor definition
     LBox(double l = 2.0, double w = 2.0, double h = 2.0) {
       cout <<"Constructor called" << endl;
       dLength = l;
       dWidth  = w;
       dHeight = h;
 
-      // Increase every time object is created
+      //! Increase every time object is created
       iObjectCount++;
     }
     double Volume() {
@@ -27,19 +27,19 @@ class LBox {
     }
 
   private:
-    double dLength;     // Length of a box
-    double dWidth;      // Width of a box
-    double dHeight;     // Height of a box
+    double dLength;     //!< Length of a box
+    double dWidth;      //!< Width of a box
+    double dHeight;     //!< Height of a box
 };
 
-// Initialize static member of class Box
+//! Initialize static member of class Box
 int LBox::iObjectCount = 0;
 
 int main(void) {
-  LBox objectBox1(3.3, 1.2, 1.5);    // Declare object box1
-  LBox objectBox2(8.5, 6.0, 2.0);    // Declare object box2
+  LBox objectBox1(3.3, 1.2, 1.5);    //!< Declare object box1
+  LBox objectBox2(8.5, 6.0, 2.0);    //!< Declare object box2
 
-  // Print total number of objects.
+  //! Print total number of objects
   cout << "Total objects: " << LBox::iObjectCount << endl;
 
   return 0;
