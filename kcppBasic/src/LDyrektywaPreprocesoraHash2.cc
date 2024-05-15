9616a17efa51178fecbb1f085cebff3ef69264fe
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2024-05-15 11:04:27 +0200

    Added example source from PISA experiment

--- kcppBasic/src/LDyrektywaPreprocesoraHash2.cc
+++ kcppBasic/src/LDyrektywaPreprocesoraHash2.cc
@@ -3,6 +3,7 @@
  * \date 2024.04.15
  * \brief Characters w C++
  * v0.01
+ * example: paper/physics/pisa/PISA.dekodowanie/pisaSource2004.2022/decoder/src/DataStore.cc
  */
 
 #include <iostream>
@@ -16,7 +17,7 @@ int main()
   c1Variable(f,Zmienna,30);
 
   fZmienna[5] = 7;
-  cout <<"Value: " fZmienna[5] << endl;
+  cout <<"Value: "<< fZmienna[5] << endl;
 
   return 0;
 }
