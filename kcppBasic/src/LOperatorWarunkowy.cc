c0b72875a239d6c956ded094141bd86b54516f89
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2024-05-15 10:01:43 +0200

    New part added to check if a number is greater or less than zero

--- kcppBasic/src/LOperatorWarunkowy.cc
+++ kcppBasic/src/LOperatorWarunkowy.cc
@@ -1,8 +1,8 @@
 /**
  * \author: Arkadiusz Bubak arkadiusz@bubak.pl
- * \date 2019.02.10
+ * \date 2019.02.10 v0.01, 2024.05.15 v0.02
  * \brief Operator warunkowy w C++
- * v0.01
+ * v0.02
  */
 
 #include <iostream>
@@ -13,6 +13,10 @@ int main()
   int x;
   cout << "Podaj liczbe calkowita:" << endl;
   cin >> x;
+  cout <<"Podałeś: "<< x << endl;
+
+
+  // ---- Liczba parzysta, nieparzysta -------------
   cout << ((x & 1) ? "Liczba nieparzysta." : "Liczba parzysta.") << endl;
 
   if((x & 1) ? 0 : 1) {
@@ -20,6 +24,16 @@ int main()
   } else {
     cout << "Liczba nieparzysta -> w else"<< endl;
   }
+
+
+  // ---- Większe od zera, mniejsze od zera -------
+  cout << ((x >= 0) ? "Liczba większa od zera." : "Liczba mniejsza od zera") << endl;
+  if((x >= 0) ? 1 : 0) {
+    cout <<"Liczba większa od zera -> w if"<< endl;
+  } else {
+    cout <<"Liczba mniejsza od zera -> w else"<< endl;
+  }
+
 
   return 0;
 }
