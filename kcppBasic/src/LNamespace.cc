280cf151099b1a9a3cc4b677e9eb8798005dad63
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-02-28 11:39:10 +0100

    Code rewritten, added doxygen c-style comment

--- kcppBasic/src/LNamespace.cc
+++ kcppBasic/src/LNamespace.cc
@@ -1,41 +1,77 @@
 /**
- * \author: Arkadiusz Bubak arkadiusz@bubak.pl
- * \date 2019.02.10
- * \brief Przestrzenie nazw w C++
- * v0.01
+ * @file LNamespace.cc
+ * @author Arkadiusz Bubak <arkadiusz@bubak.pl>
+ * @date 2019.02.10
+ * @brief Demonstration of namespaces in C++
+ * @version v0.02 (2025.02.28)
+ *
+ * This program demonstrates the use of namespaces in C++ to avoid naming
+ * conflicts and organize code into logical groups. It shows how to define and
+ * use namespaces, as well as how to resolve naming conflicts between global and
+ * namespace-scoped functions.
  */
 
 #include <iostream>
 using namespace std;
 
-//----------------------------------------------------------
+/**
+ * @namespace nasza
+ * @brief A custom namespace to demonstrate namespace usage
+ *
+ * This namespace contains a variable `k` and a function `Funkcja` to illustrate
+ * how namespaces can be used to organize code and avoid naming conflicts.
+ */
 namespace nasza {
-  int k = 7;
-  void Funkcja(){
-    cout <<"Namespace: Funkcja"<< endl;
-  }
-}
-// using namespace nasza;
-//----------------------------------------------------------
+int k = 7; /**< A variable defined within the `nasza` namespace */
 
+/**
+ * @brief A function defined within the `nasza` namespace
+ *
+ * This function prints a message to indicate that it belongs to the `nasza`
+ * namespace.
+ */
+void Funkcja() { cout << "Namespace: Funkcja" << endl; }
+} // namespace nasza
 
-void Funkcja(){
-  cout <<"Global: Funkcja"<< endl;
-}
+//----------------------------------------------------------------------
+/**
+ * @brief A global function with the same name as the one in the `nasza`
+ * namespace
+ *
+ * This function demonstrates how global functions can conflict with
+ * namespace-scoped functions if not properly resolved.
+ */
+void Funkcja() { cout << "Global: Funkcja" << endl; }
 
+//----------------------------------------------------------------------
+/**
+ * @brief Main function
+ *
+ * This program demonstrates the use of namespaces to avoid naming conflicts and
+ * organize code. It shows how to access variables and functions within a
+ * namespace and how to resolve conflicts between global and namespace-scoped
+ * functions.
+ *
+ * @return int Returns 0 on successful execution.
+ */
+int main() {
+  // Print a test message
+  cout << "Test" << endl;
 
-int main(){
-	//std::cout << "Test" << std::endl;
-	cout << "Test" << endl;
+  int k = 6; // Local variable with the same name as `nasza::k`
 
-  int k = 6;
+  // Access the variable `k` from the `nasza` namespace
+  cout << "nasza::k: " << nasza::k << endl;
 
-  cout << nasza::k << endl;
-  cout << k << endl;
+  // Access the local variable `k`
+  cout << "Local k: " << k << endl;
 
-  // Funkcja();          ///<< Error: global function in case of add 'using namespece nasz;'
-  //using namespace nasza;
+  // Call the `Funkcja` function from the `nasza` namespace
   nasza::Funkcja();
 
-	return 0;
+  // Uncommenting the following line would cause a conflict if `using namespace
+  // nasza` is active Funkcja(); // Error: Ambiguous call between global and
+  // namespace-scoped functions
+
+  return 0;
 }
