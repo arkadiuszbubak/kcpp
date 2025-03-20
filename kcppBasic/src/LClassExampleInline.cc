cc37bcfe63c0f309a9cf99bd08e890fc441ccee6
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-03-20 10:17:53 +0100

    Change of writing method

--- kcppBasic/src/LClassExampleInline.cc
+++ kcppBasic/src/LClassExampleInline.cc
@@ -3,46 +3,41 @@ mail: arkadiusz@bubak.pl
 date: 2023.03.17, v0.01
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class ClassExample
-{
-  public:
-    /** Default constructor **/
-    ClassExample() {
-      cout << "Object created" << endl;
-    };
+class ClassExample {
+public:
+  /** Default constructor **/
+  ClassExample() { cout << "Object created" << endl; };
 
-    /** Default destructor **/
-    ~ClassExample() {
-      cout << "Object deleted" << endl;
-    };
+  /** Default destructor **/
+  ~ClassExample() { cout << "Object deleted" << endl; };
 
-    /** Modifiers **/
-    inline void SetVariable(int rvariable) {
-      for(int i = 0; i < 10; i++) cout << i << endl;
-      fVariable     = rvariable;
-    };
-    // void SetVariable(int rvariable)       { fVariable     = rvariable; };
+  /** Modifiers **/
+  inline void SetVariable(int rvariable) {
+    for (int i = 0; i < 10; i++) {
+      cout << i << "\t";
+    }
+    fVariable = rvariable;
+  };
+  // void SetVariable(int rvariable)       { fVariable     = rvariable; };
 
-    /** Accesors **/
-    int GetVariable()     { return fVariable; };
+  /** Accesors **/
+  int GetVariable() { return fVariable; };
 
-  private:
-    int fVariable;
-
+private:
+  int fVariable;
 };
 
-
-int main(){
+int main() {
 
   ClassExample *object = new ClassExample();
 
   object->SetVariable(10);
-  /* object->SetVariable(10);
-   * object->SetVariable(10);
-   * object->SetVariable(10); */
+  object->SetVariable(10);
+  object->SetVariable(10);
+  object->SetVariable(10);
 
   int var = object->GetVariable();
   cout << var << endl;
