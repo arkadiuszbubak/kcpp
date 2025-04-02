e78c7cf3e44a93cd4d2202c3bfbb5a64524d9ad6
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-04-02 15:43:14 +0200

    Removed std in cout, and intentionally left std::pow

--- kcppBasic/src/ZadStudent/LPrzeciazaniePotega.cc
+++ kcppBasic/src/ZadStudent/LPrzeciazaniePotega.cc
@@ -7,7 +7,7 @@ int potega(int x, int y) { return std::pow(x, y); }
 double potega(double x, double y) { return std::pow(x, y); }
 
 int main() {
-  std::cout << potega(2, 3) << std::endl;
-  std::cout << potega(2.5, 3.0) << std::endl;
+  cout << potega(2, 3) << endl;
+  cout << potega(2.5, 3.0) << endl;
   return 0;
 }
