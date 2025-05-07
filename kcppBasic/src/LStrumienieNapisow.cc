55d40932157b449d286101bb123aa5078254eb3a
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-05-07 10:26:14 +0200

    fixed error

--- kcppBasic/src/LStrumienieNapisow.cc
+++ kcppBasic/src/LStrumienieNapisow.cc
@@ -12,22 +12,24 @@
 using namespace std;
 
 int main() {
-    long x;          // Zmienna do przechowania liczby
-    string napis;     // Zmienna do przechowania napisu
-    stringstream ss; // Strumień do napisów
+  long x;          // Zmienna do przechowania liczby
+  string napis;    // Zmienna do przechowania napisu
+  stringstream ss; // Strumień do napisów
 
-    cout << "Podaj dowolną liczbę całkowitą: ";
-    if (!(cin >> x)) {
-        cerr << "Nieprawidłowe dane wejściowe. Oczekiwano liczby całkowitej." << endl;
-        cin.clear(); // Czyścimy flagi błędów strumienia cin
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorujemy błędne dane z bufora
-        return 1; // Zwracamy kod błędu
-    }
+  cout << "Podaj dowolną liczbę całkowitą: ";
+  if (!(cin >> x)) {
+    cerr << "Nieprawidłowe dane wejściowe. Oczekiwano liczby całkowitej."
+         << endl;
+    cin.clear(); // Czyścimy flagi błędów strumienia cin
+    // cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignorujemy błędne
+    // dane z bufora
+    return 1; // Zwracamy kod błędu
+  }
 
-    ss << x;          // Do strumienia 'wysyłamy' podaną liczbę
-    napis = ss.str(); // Zamieniamy zawartość strumienia na napis
+  ss << x;          // Do strumienia 'wysyłamy' podaną liczbę
+  napis = ss.str(); // Zamieniamy zawartość strumienia na napis
 
-    cout << "Długość napisu wynosi " << napis.size() << " znaków." << endl;
+  cout << "Długość napisu wynosi " << napis.size() << " znaków." << endl;
 
-    return 0;
+  return 0;
 }
