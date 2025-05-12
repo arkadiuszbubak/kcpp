dbd06f9bfb4ed91cb6ab5d44be431f96a67883d9
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-05-12 13:24:46 +0200

    Correct uninitialized variable warning

--- kcppBasic/src/LClassZagniezdzanie.cc
+++ kcppBasic/src/LClassZagniezdzanie.cc
@@ -8,53 +8,43 @@
 #include <iostream>
 using namespace std;
 
-class Samochod
-{
+class Samochod {
+public:
+  int fZmienna;
+
+  inline void FunkcjaA() { ///< Deklaracjai jednocześnie definicja
+    cout << "FunkcjaA" << endl;
+  }
+  void FunkcjaB(); ///< Deklaracja funkcji, definicja poza klasą
+
+  //--- klasa zagnieżdżona ------
+  class Akcje {
   public:
     int fZmienna;
-
-    inline void FunkcjaA(){   ///<Deklaracjai jednocześnie definicja
-      cout <<"FunkcjaA"<< endl;
-    }
-    void FunkcjaB();          ///< Deklaracja funkcji, definicja poza klasą
-
-    //--- klasa zagnieżdżona ------
-    class Akcje
-    {
-      public:
-        int fZmienna;
-    };
-    //-------------------------------
+  };
+  //-------------------------------
 };
 
-
+//-----------------------------------------------------------
+void Samochod::FunkcjaB() { cout << "FunkcjaB" << endl; }
 
 //-----------------------------------------------------------
-void Samochod::FunkcjaB(){
-  cout <<"FunkcjaB" << endl;
-}
-
-
-
-//-----------------------------------------------------------
-int main()
-{
+int main() {
   Samochod Volvo;
   Volvo.fZmienna = 7;
 
   Samochod *Ford = new Samochod();
   Ford->fZmienna = 8;
 
-  //Akcje Trab;                 //Error
-  //Trab.fZmienna = 5;         //Error
+  // Akcje Trab;                 //Error
+  // Trab.fZmienna = 5;         //Error
 
-
-  Samochod::Akcje Trab;
-  Trab.fZmienna = 5;
+  Samochod::Akcje *Trab = new Samochod::Akcje();
+  Trab->fZmienna = 5;
 
   cout << Volvo.fZmienna << endl;
   cout << Ford->fZmienna << endl;
-  //cout << Trab.fZmienna  << endl;
+  // cout << Trab.fZmienna  << endl;
   Volvo.FunkcjaA();
   Ford->FunkcjaB();
 }
