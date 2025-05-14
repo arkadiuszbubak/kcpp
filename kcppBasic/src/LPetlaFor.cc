db41e47f0a6e182cf3d25050d575bcff4e128de4
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-05-14 09:10:20 +0200

    new example of the for loop with two variables

--- kcppBasic/src/LPetlaFor.cc
+++ kcppBasic/src/LPetlaFor.cc
@@ -33,9 +33,21 @@ int main() {
   cout << "\n";
 
   // 4. Pętla z wieloma zmiennymi
-  cout << "Pętla z dwoma zmiennymi:\n";
+  cout << "Pętla z dwoma zmiennymi (1):\n";
   for (int i = 0, j = 10; i < 5; i++, j -= 2) {
     cout << "i: " << i << ", j: " << j << "\n";
+  }
+  cout << "\n";
+
+  cout << "Pętla z dwoma zmiennymi (2):\n";
+  for (int i = 0, j = 10; i <= j; i++, j--) {
+    cout << "i: " << i << ", j: " << j << endl;
+  }
+  cout << "\n";
+
+  cout << "Pętla z dwoma zmiennymi (3) i różnymi operacjami na zmiennych:\n";
+  for (int x = 1, y = 1; x < 10 && y < 20; x += 2, y *= 2) {
+    cout << "x: " << x << ", y: " << y << endl;
   }
   cout << "\n";
 
