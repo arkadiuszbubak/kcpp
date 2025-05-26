6e1e19a5be3e9fb1e198118de02b4d8cb348fe67
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2025-05-26 09:29:38 +0200

    Cleaning

--- kcppBasic/src/LClassKonstruktorKopiujacy.cc
+++ kcppBasic/src/LClassKonstruktorKopiujacy.cc
@@ -8,73 +8,62 @@
 #include <iostream>
 using namespace std;
 
-class A 
-{   
- public:
-   A(int x, int y) : fX(x), fY(y) {}
-   /* A(int x, int y){
-    *    fX = x;
-    *    fY = y;
-    * } */
-   A(int x){
-       fX = x;
-   }
-   A() {
-      fX = fY = 0; 
-   }
+class A {
+public:
+  A(int x, int y) : fX(x), fY(y) {}
+  /* A(int x, int y){
+   *    fX = x;
+   *    fY = y;
+   * } */
+  A(int x) { fX = x; }
+  A() { fX = fY = 0; }
 
-   /** Konstruktor kopiujacy **/
-   // A co jesli go wcale nie zdefiniujemy? 
-   A(const A &c) {
-      fX = c.fX;
-      fY = c.fY;
-   }
+  /** Konstruktor kopiujacy **/
+  // A co jesli go wcale nie zdefiniujemy?
+  A(const A &c) {
+    fX = c.fX;
+    fY = c.fY;
+  }
 
-   void Wypisz();
-   
-   int fX, fY;
+  void Wypisz();
+
+  int fX, fY;
 };
 
-A c0(3, 6);  //statyczny
-  
-void A::Wypisz(){
-   cout <<"fX: "<< fX << endl;
-   cout <<"fY: "<< fY << endl;
+A c0(3, 6); // statyczny
+
+void A::Wypisz() {
+  cout << "fX: " << fX << endl;
+  cout << "fY: " << fY << endl;
 }
 
+int main() {
+  //--------- automatyczne -------
+  A c(7);
+  A c1(4, -1);
+  A c2;
+  A c3 = c1; ///< kopiujący <=> c3(c1)
+  // A c3(c1);
 
-int main()
-{
-   //--------- automatyczne -------
-   A c(7);
-   A c1(4, -1);
-   A c2;
-   A c3 = c1; ///< kopiujący <=> c3(c1)
-   // A c3(c1);
-  
-   
-   cout << "Wypis danych z obiektu c1"<<endl;
-   c1.Wypisz();
-   cout << "Wypis danych z obiektu c3"<<endl;
-   c3.Wypisz();
+  cout << "Wypis danych z obiektu c1" << endl;
+  c1.Wypisz();
+  cout << "Wypis danych z obiektu c3" << endl;
+  c3.Wypisz();
 
-   
+  //-------- dynamiczne ----------
+  A *p1, *p2, *p3;
+  p1 = new A(4, -1);
+  p2 = new A();
+  p3 = new A(*p1);
 
-   //-------- dynamiczne ----------
-   A *p1, *p2, *p3;
-   p1 = new A(4, -1);
-   p2 = new A();
-   p3 = new A(*p1);
+  cout << "\nWypis danych z obiektu p1" << endl;
+  p1->Wypisz();
+  cout << "Wypis danych z obiektu p3" << endl;
+  p3->Wypisz();
 
-   cout << "\nWypis danych z obiektu p1"<<endl;
-   p1->Wypisz();
-   cout << "Wypis danych z obiektu p3"<<endl;
-   p3->Wypisz();
-   
-   delete p1;
-   delete p2;
-   delete p3;
-   
-   return 0;
+  delete p1;
+  delete p2;
+  delete p3;
+
+  return 0;
 }
-
