0896661640a0c613c9d6cbc56f882ceb971a0d8a
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2026-03-20 12:15:26 +0100

    Cleaning

--- kcppKviCar/kviCarCMake/include/KviVan.h
+++ kcppKviCar/kviCarCMake/include/KviVan.h
@@ -2,23 +2,21 @@
    mail: arkadiusz@buabk.pl
    date: 2008.02.22  v0.01
          2019.02.27. v0.02
+         2026.03.20. v0.03
  */
- 
+
 #ifndef KVIVAN_H
 #define KVIVAN_H 1
 
 #include "KviCar.h"
 
-class KviVan : public KviCar
-{
- public:
+class KviVan : public KviCar {
+public:
   KviVan();
   ~KviVan();
 
   void StealPlate();
   void MakeRobbery();
-
-  //ClassDef(KviVan,1)
 };
 
 #endif
