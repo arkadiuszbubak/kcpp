fde6a2d168407c84bebb624930e7957e0870652c
arkadiuszbubak <arkadiusz.bubak@us.edu.pl>
2026-05-07 09:32:21 +0200

    Removed filename. Added option to remove file

--- kcppBasic/src/LStrumieniePlikoweA.cc
+++ kcppBasic/src/LStrumieniePlikoweA.cc
@@ -5,24 +5,26 @@
  * v0.02
  */
 
+#include <filesystem> //C++17
 #include <fstream>
 #include <iostream>
 #include <string>
 
 using namespace std;
+// namespace fs = std::filesystem;
 
 int main() {
 
   // ----- Przykład zapisu do pliku ------------------
-  ofstream f("log.txt", ios::app); // Otwieramy w trybie dopisywania (ios::app)
+  string fileName = "log.txt";
+  ofstream f(fileName, ios::app); // Otwieramy w trybie dopisywania (ios::app)
 
   if (!f.is_open()) {
-    cerr << "Nie można otworzyć pliku log.txt!" << endl;
+    cerr << "Nie można otworzyć pliku" << fileName << endl;
     return 1; // Zwracamy kod błędu
   }
 
   string fullName;
-
   cout << "Wpisz swoje pełne imię i nazwisko (naciśnij Enter): \n";
   getline(cin >> ws, fullName); // Używamy getline do wczytania całej linii, >>
                                 // ws usuwa białe znaki z początku
@@ -31,25 +33,41 @@ int main() {
 
   f << fullName << endl; // Dodajemy znak nowej linii po zapisie
   if (f.fail()) {
-    cerr << "Błąd podczas zapisu do pliku log.txt!" << endl;
+    cerr << "Błąd podczas zapisu do pliku " << fileName << endl;
   } else {
-    cout << "Twoje imię zostało zapisane do pliku log.txt" << endl;
+    cout << "Twoje imię zostało zapisane do pliku " << fileName << endl;
   }
-
   f.close();
 
   // ----- Przykład czytania z pliku ------------------
-  ifstream plik("log.txt");
+  ifstream plik(fileName);
   string linia;
 
   if (plik.is_open()) {
     while (getline(plik, linia)) { // Czytaj dopóki są linie
-      cout << "Twoje imię zostało odczytane z pliku log.txt" << endl;
+      cout << "Twoje imię zostało odczytane z pliku " << fileName << endl;
       cout << linia << endl;
     }
     plik.close();
   } else {
-    cerr << "Nie udało się otworzyć pliku!";
+    cerr << "Nie udało się otworzyć pliku " << fileName << endl;
+  }
+
+  // ----- Usuwanie pliku ----------------------------
+  // C++17
+  if (std::filesystem::remove(fileName)) {
+    cout << "Plik " << fileName << " został pomyślnie usunięty." << endl;
+  } else {
+    cout << "Plik " << fileName << " nie istnieje lub nie mógł zostać usunięty."
+         << endl;
+  }
+
+  // Przed C++17: std::remove returns 0 on success
+  if (std::remove("log.txt") == 0) {
+    cout << "Plik " << fileName << " został pomyślnie usunięty.\n";
+  } else {
+    // perror prints the specific reason for failure
+    perror("Plik log.txt nie istnieje lub nie mógł zostać usunięty.");
   }
 
   return 0;
